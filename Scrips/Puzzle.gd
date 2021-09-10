15c6dc989087ba2600a02c1f3c3f0b7cd3f1f604
Ant0yne <antoine.soliv@gmail.com>
2021-09-10 16:47:18 +0200

    polish boucles

--- Scrips/Puzzle.gd
+++ Scrips/Puzzle.gd
@@ -26,6 +26,14 @@ onready var listes = Listes
 onready var ecranNoir = $"Ecran noir"
 
 var delay = int(0)
+var monte_son = int(-80)
+var guitare_max = bool(false)
+var melodie_max = bool(false)
+var accord_max = bool(false)
+var basse_max = bool(false)
+var hihat_max = bool(false)
+var rimshot_max = bool(false)
+var kick_max = bool(false)
 
 func _ready():
 	bouclesGuitareIntro.play(0)
@@ -61,6 +69,57 @@ func _process(delta):
 				ecranNoir.modulate.a += 0.01
 			if ecranNoir.modulate.a >= 1 :
 				get_tree().quit()
+	
+	if frame_compteur.boucle_guitare_joue == true and guitare_max == false:
+		if bouclesGuitare.volume_db < 0:
+			monte_son += 1
+			bouclesGuitare.set_volume_db(monte_son)
+		if bouclesGuitare.volume_db >= 0:
+			monte_son = -80
+			guitare_max = true
+	if frame_compteur.boucle_melodie_joue == true and melodie_max == false:
+		if bouclesMelodie.volume_db < 0:
+			monte_son += 1
+			bouclesMelodie.set_volume_db(monte_son)
+		if bouclesMelodie.volume_db >= 0:
+			monte_son = -80
+			melodie_max = true
+	if frame_compteur.boucle_accord_joue == true and accord_max == false:
+		if bouclesAccord.volume_db < 0:
+			monte_son += 1
+			bouclesAccord.set_volume_db(monte_son)
+		if bouclesAccord.volume_db >= 0:
+			monte_son = -80
+			accord_max = true
+	if frame_compteur.boucle_basse_joue == true and basse_max == false:
+		if bouclesBasse.volume_db < 0:
+			monte_son += 1
+			bouclesBasse.set_volume_db(monte_son)
+		if bouclesBasse.volume_db >= 0:
+			monte_son = -80
+			basse_max = true
+	if frame_compteur.boucle_hihat_joue == true and hihat_max == false:
+		if bouclesHihat.volume_db < 0:
+			monte_son += 1
+			bouclesHihat.set_volume_db(monte_son)
+		if bouclesHihat.volume_db >= 0:
+			monte_son = -80
+			hihat_max = true
+	if frame_compteur.boucle_rimshot_joue == true and rimshot_max == false:
+		if bouclesRimshot.volume_db < 0:
+			monte_son += 1
+			bouclesRimshot.set_volume_db(monte_son)
+		if bouclesRimshot.volume_db >= 0:
+			monte_son = -80
+			rimshot_max = true
+	if frame_compteur.boucle_kick_joue == true and kick_max == false:
+		if bouclesKick.volume_db < 0:
+			monte_son += 1
+			bouclesKick.set_volume_db(monte_son)
+		if bouclesKick.volume_db >= 0:
+			monte_son = -80
+			kick_max = true
+
 
 func _input(event):
 	if event.is_action_released("ui_cancel"):
@@ -112,34 +171,16 @@ func _piece_pose():
 
 func _boucle_joue():
 	if frame_compteur.boucle_guitare >= 7 and frame_compteur.boucle_guitare_joue == false :
-		bouclesGuitare.set_volume_db(0)
 		frame_compteur.boucle_guitare_joue = true
 	if frame_compteur.boucle_melodie >= 7 and frame_compteur.boucle_melodie_joue == false :
-		bouclesMelodie.set_volume_db(0)
 		frame_compteur.boucle_melodie_joue = true
 	if frame_compteur.boucle_accord >= 7 and frame_compteur.boucle_accord_joue == false :
-		bouclesAccord.set_volume_db(0)
 		frame_compteur.boucle_accord_joue = true
 	if frame_compteur.boucle_basse >= 7 and frame_compteur.boucle_basse_joue == false :
-		bouclesBasse.set_volume_db(0)
 		frame_compteur.boucle_basse_joue = true
 	if frame_compteur.boucle_hihat >= 7 and frame_compteur.boucle_hihat_joue == false :
-		bouclesHihat.set_volume_db(0)
 		frame_compteur.boucle_hihat_joue = true
 	if frame_compteur.boucle_rimshot >= 7 and frame_compteur.boucle_rimshot_joue == false :
-		bouclesRimshot.set_volume_db(0)
 		frame_compteur.boucle_rimshot_joue = true
 	if frame_compteur.boucle_kick >= 7 and frame_compteur.boucle_kick_joue == false :
-		bouclesKick.set_volume_db(0)
 		frame_compteur.boucle_kick_joue = true
-
-#func _input(event):
-#	if event.is_action_released("ui_down"):
-#		mixingDesk.fade_in("Boucles", "Boucles Guitare")
-#	if event.is_action_released("ui_right"):
-#		mixingDesk.toggle_fade("Boucles", "Boucles Basse")
-#	if event.is_action_released("ui_left"):
-#		mixingDesk.toggle_fade("Boucles", "Boucles Rimshot")
-#	if event.is_action_released("ui_up"):
-#		mixingDesk.toggle_fade("Boucles", "Boucles Kick")
-
